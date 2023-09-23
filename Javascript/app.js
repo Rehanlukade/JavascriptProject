2984f3b1fa15200a6e04909729c7f43f8f53a86c
Rehan Lukade <47839836+Rehanlukade@users.noreply.github.com>
2023-09-23 21:45:28 +0530

    Update app.js

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,473 +1,377 @@
-const food_chart = [
+let fooditems= [
   {
-    id: 1,
-    foodname: "Apple",
-    calorie: 95,
-    category: "Fruit",
-    protiens: 0.3,
-    cab: 25,
+    "id": 1,
+    "foodname": "Apple",
+    "calorie": 95,
+    "category": "Fruit",
+    "protiens": 0.3,
+    "cab": 25
   },
   {
-    id: 2,
-    foodname: "Banana",
-    calorie: 105,
-    category: "Fruit",
-    protiens: 1.3,
-    cab: 27,
+    "id": 2,
+    "foodname": "Banana",
+    "calorie": 105,
+    "category": "Fruit",
+    "protiens": 1.3,
+    "cab": 27
   },
   {
-    id: 3,
-    foodname: "Orange",
-    calorie: 62,
-    category: "Fruit",
-    protiens: 1.2,
-    cab: 15,
+    "id": 3,
+    "foodname": "Orange",
+    "calorie": 62,
+    "category": "Fruit",
+    "protiens": 1.2,
+    "cab": 15
   },
   {
-    id: 4,
-    foodname: "Pear",
-    calorie: 95,
-    category: "Fruit",
-    protiens: 0.5,
-    cab: 25,
+    "id": 4,
+    "foodname": "Pear",
+    "calorie": 95,
+    "category": "Fruit",
+    "protiens": 0.5,
+    "cab": 25
   },
   {
-    id: 5,
-    foodname: "Grapefruit",
-    calorie: 42,
-    category: "Fruit",
-    protiens: 1.1,
-    cab: 11,
+    "id": 5,
+    "foodname": "Grapefruit",
+    "calorie": 42,
+    "category": "Fruit",
+    "protiens": 1.1,
+    "cab": 11
   },
   {
-    id: 6,
-    foodname: "Strawberry",
-    calorie: 46,
-    category: "Fruit",
-    protiens: 1.5,
-    cab: 10,
+    "id": 6,
+    "foodname": "Strawberry",
+    "calorie": 46,
+    "category": "Fruit",
+    "protiens": 1.5,
+    "cab": 10
   },
   {
-    id: 7,
-    foodname: "Blueberry",
-    calorie: 52,
-    category: "Fruit",
-    protiens: 1.1,
-    cab: 14,
+    "id": 7,
+    "foodname": "Blueberry",
+    "calorie": 52,
+    "category": "Fruit",
+    "protiens": 1.1,
+    "cab": 14
   },
   {
-    id: 8,
-    foodname: "Raspberry",
-    calorie: 52,
-    category: "Fruit",
-    protiens: 1.4,
-    cab: 15,
+    "id": 8,
+    "foodname": "Raspberry",
+    "calorie": 52,
+    "category": "Fruit",
+    "protiens": 1.4,
+    "cab": 15
   },
   {
-    id: 9,
-    foodname: "Broccoli",
-    calorie: 34,
-    category: "Vegetable",
-    protiens: 3.3,
-    cab: 5,
+    "id": 9,
+    "foodname": "Broccoli",
+    "calorie": 34,
+    "category": "Vegetable",
+    "protiens": 3.3,
+    "cab": 5
   },
   {
-    id: 10,
-    foodname: "Cauliflower",
-    calorie: 25,
-    category: "Vegetable",
-    protiens: 2.6,
-    cab: 5,
+    "id": 10,
+    "foodname": "Cauliflower",
+    "calorie": 25,
+    "category": "Vegetable",
+    "protiens": 2.6,
+    "cab": 5
   },
   {
-    id: 11,
-    foodname: "Green Beans",
-    calorie: 31,
-    category: "Vegetable",
-    protiens: 2.4,
-    cab: 4,
+    "id": 11,
+    "foodname": "Green Beans",
+    "calorie": 31,
+    "category": "Vegetable",
+    "protiens": 2.4,
+    "cab": 4
   },
   {
-    id: 12,
-    foodname: "Asparagus",
-    calorie: 20,
-    category: "Vegetable",
-    protiens: 2.2,
-    cab: 2,
+    "id": 12,
+    "foodname": "Asparagus",
+    "calorie": 20,
+    "category": "Vegetable",
+    "protiens": 2.2,
+    "cab": 2
   },
   {
-    id: 13,
-    foodname: "Spinach",
-    calorie: 23,
-    category: "Vegetable",
-    protiens: 3.5,
-    cab: 4,
+    "id": 13,
+    "foodname": "Spinach",
+    "calorie": 23,
+    "category": "Vegetable",
+    "protiens": 3.5,
+    "cab": 4
   },
   {
-    id: 14,
-    foodname: "Kale",
-    calorie: 33,
-    category: "Vegetable",
-    protiens: 4.3,
-    cab: 7,
+    "id": 14,
+    "foodname": "Kale",
+    "calorie": 33,
+    "category": "Vegetable",
+    "protiens": 4.3,
+    "cab": 7
   },
   {
-    id: 15,
-    foodname: "Sweet Potato",
-    calorie: 103,
-    category: "Vegetable",
-    protiens: 2,
-    cab: 27,
+    "id": 15,
+    "foodname": "Sweet Potato",
+    "calorie": 103,
+    "category": "Vegetable",
+    "protiens": 2,
+    "cab": 27
   },
   {
-    id: 16,
-    foodname: "Potato",
-    calorie: 161,
-    category: "Vegetable",
-    protiens: 4.3,
-    cab: 37,
+    "id": 16,
+    "foodname": "Potato",
+    "calorie": 161,
+    "category": "Vegetable",
+    "protiens": 4.3,
+    "cab": 37
+  },
+  {"id": 17,
+    "foodname": "Carrot",
+    "calorie": 41,
+    "category": "Vegetable",
+    "protiens": 0.9,
+    "cab": 9
   },
   {
-    id: 17,
-    foodname: "Carrot",
-    calorie: 41,
-    category: "Vegetable",
-    protiens: 0.9,
-    cab: 9,
+    "id": 18,
+    "foodname": "Onion",
+    "calorie": 40,
+    "category": "Vegetable",
+    "protiens": 1.4,
+    "cab": 9
   },
   {
-    id: 18,
-    foodname: "Onion",
-    calorie: 40,
-    category: "Vegetable",
-    protiens: 1.4,
-    cab: 9,
+    "id": 19,
+    "foodname": "Egg",
+    "calorie": 77,
+    "category": "Protein",
+    "protiens": 6.3,
+    "cab": 0.5
   },
   {
-    id: 19,
-    foodname: "Egg",
-    calorie: 77,
-    category: "Protein",
-    protiens: 6.3,
-    cab: 0.5,
+    "id": 20,
+    "foodname": "Chicken Breast",
+    "calorie": 165,
+    "category": "Protein",
+    "protiens": 31,
+    "cab": 0
   },
   {
-    id: 20,
-    foodname: "Chicken Breast",
-    calorie: 165,
-    category: "Protein",
-    protiens: 31,
-    cab: 0,
+    "id": 21,
+    "foodname": "Salmon",
+    "calorie": 206,
+    "category": "Protein",
+    "protiens": 22,
+    "cab": 0
   },
   {
-    id: 21,
-    foodname: "Salmon",
-    calorie: 206,
-    category: "Protein",
-    protiens: 22,
-    cab: 0,
+    "id": 22,
+    "foodname": "Tuna",
+    "calorie": 179,
+    "category": "Protein",
+    "protiens": 39,
+    "cab": 0
   },
   {
-    id: 22,
-    foodname: "Tuna",
-    calorie: 179,
-    category: "Protein",
-    protiens: 39,
-    cab: 0,
+    "id": 23,
+    "foodname": "Beef",
+    "calorie": 250,
+    "category": "Protein",
+    "protiens": 26,
+    "cab": 0
   },
   {
-    id: 23,
-    foodname: "Beef",
-    calorie: 250,
-    category: "Protein",
-    protiens: 26,
-    cab: 0,
+    "id": 24,
+    "foodname": "Pork",
+    "calorie": 242,
+    "category": "Protein",
+    "protiens": 26,
+    "cab": 0
   },
   {
-    id: 24,
-    foodname: "Pork",
-    calorie: 242,
-    category: "Protein",
-    protiens: 26,
-    cab: 0,
+    "id": 25,
+    "foodname": "Lamb",
+    "calorie": 294,
+    "category": "Protein",
+    "protiens": 25,
+    "cab": 0
   },
   {
-    id: 25,
-    foodname: "Lamb",
-    calorie: 294,
-    category: "Protein",
-    protiens: 25,
-    cab: 0,
+    "id": 26,
+    "foodname": "Shrimp",
+    "calorie": 85,
+    "category": "Protein",
+    "protiens": 20,
+    "cab": 0.2
   },
   {
-    id: 26,
-    foodname: "Shrimp",
-    calorie: 85,
-    category: "Protein",
-    protiens: 20,
-    cab: 0.2,
+    "id": 27,
+    "foodname": "Quinoa",
+    "calorie": 120,
+    "category": "Grain",
+    "protiens": 4.4,
+    "cab": 21
   },
   {
-    id: 27,
-    foodname: "Quinoa",
-    calorie: 120,
-    category: "Grain",
-    protiens: 4.4,
-    cab: 21,
+    "id": 28,
+    "foodname": "Brown Rice",
+    "calorie": 216,
+    "category": "Grain",
+    "protiens": 4.5,
+    "cab": 45
   },
   {
-    id: 28,
-    foodname: "Brown Rice",
-    calorie: 216,
-    category: "Grain",
-    protiens: 4.5,
-    cab: 45,
+    "id": 29,
+    "foodname": "Oats",
+    "calorie": 389,
+    "category": "Grain",
+    "protiens": 16.9,
+    "cab": 66
   },
   {
-    id: 29,
-    foodname: "Oats",
-    calorie: 389,
-    category: "Grain",
-    protiens: 16.9,
-    cab: 66,
+    "id": 30,
+    "foodname": "Quinoa",
+    "calorie": 120,
+    "category": "Grain",
+    "protiens": 4.4,
+    "cab": 21
   },
   {
-    id: 30,
-    foodname: "Quinoa",
-    calorie: 120,
-    category: "Grain",
-    protiens: 4.4,
-    cab: 21,
+    "id": 31,
+    "foodname": "Bread",
+    "calorie": 265,
+    "category": "Grain",
+    "protiens": 9.4,
+    "cab": 49
   },
   {
-    id: 31,
-    foodname: "Bread",
-    calorie: 265,
-    category: "Grain",
-    protiens: 9.4,
-    cab: 49,
+    "id": 32,
+    "foodname": "Pasta",
+    "calorie": 131,
+    "category": "Grain",
+    "protiens": 5.5,
+    "cab": 26
   },
   {
-    id: 32,
-    foodname: "Pasta",
-    calorie: 131,
-    category: "Grain",
-    protiens: 5.5,
-    cab: 26,
+    "id": 33,
+    "foodname": "Milk",
+    "calorie": 60,
+    "category": "Dairy",
+    "protiens": 3.2,
+    "cab": 5.1
   },
   {
-    id: 33,
-    foodname: "Milk",
-    calorie: 60,
-    category: "Dairy",
-    protiens: 3.2,
-    cab: 5.1,
+    "id": 34,
+    "foodname": "Cheese",
+    "calorie": 402,
+    "category": "Dairy",
+    "protiens": 25,
+    "cab": 2.4
   },
   {
-    id: 34,
-    foodname: "Cheese",
-    calorie: 402,
-    category: "Dairy",
-    protiens: 25,
-    cab: 2.4,
+    "id": 35,
+    "foodname": "Yogurt",
+    "calorie": 59,
+    "category": "Dairy",
+    "protiens": 3.5,
+    "cab": 5
   },
   {
-    id: 35,
-    foodname: "Yogurt",
-    calorie: 59,
-    category: "Dairy",
-    protiens: 3.5,
-    cab: 5,
+    "id": 36,
+    "foodname": "Butter",
+    "calorie": 717,
+    "category": "Dairy",
+    "protiens": 0.9,
+    "cab": 0.1
   },
   {
-    id: 36,
-    foodname: "Butter",
-    calorie: 717,
-    category: "Dairy",
-    protiens: 0.9,
-    cab: 0.1,
+    "id": 37,
+    "foodname": "Almonds",
+    "calorie": 579,
+    "category": "Nuts",
+    "protiens": 21,
+    "cab": 22
   },
   {
-    id: 37,
-    foodname: "Almonds",
-    calorie: 579,
-    category: "Nuts",
-    protiens: 21,
-    cab: 22,
+    "id": 38,
+    "foodname": "Walnuts",
+    "calorie": 654,
+    "category": "Nuts",
+    "protiens": 15,
+    "cab": 14
   },
   {
-    id: 38,
-    foodname: "Walnuts",
-    calorie: 654,
-    category: "Nuts",
-    protiens: 15,
-    cab: 14,
+    "id": 39,
+    "foodname": "Peanuts",
+    "calorie": 567,
+    "category": "Nuts",
+    "protiens": 26,
+    "cab": 16
   },
   {
-    id: 39,
-    foodname: "Peanuts",
-    calorie: 567,
-    category: "Nuts",
-    protiens: 26,
-    cab: 16,
-  },
-  {
-    id: 40,
-    foodname: "Cashews",
-    calorie: 553,
-    category: "Nuts",
-    protiens: 18,
-    cab: 30,
-  },
-];
-
-// List all the food items
-function ListAllFoodItems() {
-  let count = 0;
-
-  for (let item = 0; item < food_chart.length; item++) {
-    console.log(food_chart[item]);
+    "id": 40,
+    "foodname": "Cashews",
+    "calorie": 553,
+    "category": "Nuts",
+    "protiens": 18,
+    "cab": 30
   }
-  console.log("\n", "Total Items :- ", count);
-}
+]
 
-// list all the food items with category vegetables
-function ListAllVegetableItems() {
-  let count = 0;
 
-  for (let item = 0; item < food_chart.length; item++) {
-    if (food_chart[item].category === "Vegetable") {
-      console.log(food_chart[item]);
-    }
-  }
-  console.log("\n", "Total Vegetable Items :- ", count);
-}
+// 1 list all the food items
 
-// list all the food items with category fruit
-function ListAllFruitItems() {
-  let count = 0;
+let allfooditems= fooditems.map((x) => x);
+console.log("Allfooditems",allfooditems);
 
-  for (let item = 0; item < food_chart.length; item++) {
-    if (food_chart[item].category === "Fruit") {
-      console.log(food_chart[item]);
-      count++;
-    }
-  }
-  console.log("\n", "Total Fruit Items:- ", count);
-}
+// 2 list all the food items with category vegetables
 
-// list all the food items with category protien
-function ListAllProteinItems() {
-  let count = 0;
-  for (let item = 0; item < food_chart.length; item++) {
-    if (food_chart[item].category === "Protein") {
-      console.log(food_chart[item]);
-      count++;
-    }
-  }
-  console.log("\n", "Total Protein Items :- ", count);
-}
+let categoryvagetables=fooditems.filter((items) => items.category==="Vegetable");
+console.log("food items with category vegetables",categoryvagetables)
 
-// list all the food items with category nuts
-function ListAllNutsItems() {
-  let count = 0;
-  for (let item = 0; item < food_chart.length; item++) {
-    if (food_chart[item].category === "Nuts") {
-      console.log(food_chart[item]);
-      count++;
-    }
-  }
-  console.log("\n", "Total Nuts Items :- ", count);
-}
+ // 3 list all the food items with category fruit
+ 
+let categoryfruit=fooditems.filter((items) => items.category==="Fruit");
+console.log("food items with category fruit",categoryfruit)
 
-// list all the food items with category grains
-function ListAllGrainsItems() {
-  let count = 0;
-  for (let item = 0; item < food_chart.length; item++) {
-    if (food_chart[item].category === "Grain") {
-      console.log(food_chart[item]);
-      count++;
-    }
-  }
-  console.log("\n", "Total Grain Items:- ", count);
-}
+// 4 list all the food items with category Nuts
 
-// list all the food items with category dairy
-function ListAllDairyItems() {
-  let count = 0;
-  for (let item = 0; item < food_chart.length; item++) {
-    if (food_chart[item].category === "Dairy") {
-      console.log(food_chart[item]);
-      count++;
-    }
-  }
-  console.log("\n", "Total Dairy Items :- ", count);
-}
+let categorynuts=fooditems.filter((items) => items.category==="Nuts");
+console.log("food items with category nuts",categorynuts)
 
-// list all the food items with calorie above 100
-function FilterHighCalorieFood() {
-  let count = 0;
-  for (let item = 0; item < food_chart.length; item++) {
-    if (food_chart[item].calorie > 100) {
-      console.log(food_chart[item]);
-    }
-    count++;
-  }
+// 5 list all the food items with category grains
 
-  console.log("\n", "Total Items Above 100 Calorie :- ", count);
-}
+let categorygrains=fooditems.filter((items) => items.category==="Grain");
+console.log("food items with category grains",categorygrains)
 
-// list all the food items with calorie below 100
-function FilterLowerCalorieFood() {
-  let count = 0;
-  for (let item = 0; item < food_chart.length; item++) {
-    if (food_chart[item].calorie < 100) {
-      console.log(food_chart[item]);
-    }
-    count++;
-  }
+// 6 list all the food items with category dairy
 
-  console.log("\n", "Total Items Below 100 Calorie :- ", count);
-}
+let categorydairy=fooditems.filter((items) => items.category==="Dairy");
+console.log("food items with category dairy",categorydairy)
 
-// list all the food items with highest protien content to lowest
+// 7 list all the food items with calorie above 100
 
-function FilterHighToLowProteinItems() {
-  function swapping(obj1, obj2) {
-    for (let prop in obj1) {
-      [obj1[prop], obj2[prop]] = [obj2[prop], obj1[prop]];
-    }
-  }
-  for (let i = 0; i < food_chart.length; i++) {
-    for (let j = i + 1; j < food_chart.length; j++) {
-      if (food_chart[i].protiens < food_chart[j].protiens) {
-        swapping(food_chart[i], food_chart[j]);
-      }
-    }
-  }
+let calorieabove100=fooditems.filter((items) => items.calorie>100);
+console.log("Food items calorie above 100",calorieabove100);
 
-  for (let item = 0; item < food_chart.length; item++) {
-    console.log(food_chart[item]);
-  }
-}
+// 8 list all the food items with calorie below 100
 
-// list all the food items with lowest cab content to highest
-function FilterLowToHighCabItems() {
-  function swapping(obj1, obj2) {
-    for (let prop in obj1) {
-      [obj1[prop], obj2[prop]] = [obj2[prop], obj1[prop]];
-    }
-  }
+let caloriebelow100=fooditems.filter((items) => items.calorie<100);
+console.log("Food items calorie below 100",caloriebelow100);
 
-  for (let i = 0; i < food_chart.length; i++) {
-    for (let j = i + 1; j < food_chart.length; j++) {
-      if (food_chart[i].cab > food_chart[j].cab) {
-        swapping(food_chart[i], food_chart[j]);
-      }
-    }
-  }
+// 9 list all the food items with highest protien content to lowest
 
-  for (let item = 0; item < food_chart.length; item++) {
-    console.log(food_chart[item]);
-  }
-}
+fooditems.sort((a, b) => b.protiens - a.protiens);
+const highestprotien  = fooditems.map((fooditem) => fooditem);
+console.log("food items with highest protien content to lowest,highest protien",highestprotien);
+
+// 10  list all the food items with lowest cab content to highest
+
+fooditems.sort((a, b) => a.cab - b.cab);
+const lowestcap = fooditems.map((fooditem) => fooditem);
+console.log("food items with lowest cab content to highest",lowestcap);
+
+
+
